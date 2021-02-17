31daa004ad860b00b3c836de8b0e78f281e7b9c7
José Vitor Gonçalves de Melo <josemelo1904003@gmail.com>
2021-02-17 15:50:30 +0000

    Registration window - Create the main structure the window register

---
+++ views/windowRegister.py
@@ -0,0 +1,7 @@
+def register():
+  print("==================================================")
+  print("|                                                |")
+  print("|                 Pizzaria Luigi                 |")
+  print("|                Tela de cadastro                |")
+  print("==================================================")
+  print("Informe seus dados abaixo para fazer o cadastro!\n")
